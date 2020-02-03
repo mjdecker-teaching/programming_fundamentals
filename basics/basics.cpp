2f5c0aeeda19f9fabd1f6734149235ddbf3dd7c5
Michael J. Decker, Ph.D <mdecke@bgsu.edu>
2020-02-03 11:32:24 -0800

    Add basics file

---
+++ basics/basics.cpp
@@ -0,0 +1,24 @@
+/**
+ * @file basics.cpp
+ *
+ * Illustrate basic types, printing, and arithmetic.
+ *
+ * @author Michae J Decker, Ph.D.
+ * @date 2/3/2020
+ */
+
+#include <iostream>
+
+/*
+
+ Block/multiline comment
+
+*/
+int main() {
+
+    // single line comment
+
+    std::cout << "Hello World!\n";
+
+    return 0;
+}
