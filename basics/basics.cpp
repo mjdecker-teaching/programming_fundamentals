9d67290b36105fa7e741bf965525442e8afe880c
Michael J. Decker, Ph.D <mdecke@bgsu.edu>
2020-02-03 12:57:45 -0800

    Add illustration for basic types

--- basics/basics.cpp
+++ basics/basics.cpp
@@ -20,10 +20,33 @@ int main() {
 
     std::cout << "Hello World!\n";
 
-    int number = 42;
+    short smaller_number = 10;
+    int number = -250;
+    unsigned int non_negative_number = -1;
+    long larger_number = 500;
 
+    std::cout << smaller_number << "\n";
     std::cout << number << "\n";
+    std::cout << larger_number << "\n";
+    std::cout << non_negative_number << "\n";
+  
+    std::cout << "An short is: " << sizeof(smaller_number) << " bytes\n";
+    std::cout << "An int is: " << sizeof(number) << " bytes\n";
+    std::cout << "An unsigned int is: "<< sizeof(non_negative_number) << " bytes\n";
+    std::cout << "An long is: "<< sizeof(larger_number) << " bytes\n";
 
+    char character = 'a';
+    int num = 'a';
+    std::cout << character << '\n';
+    std::cout << num << '\n';
+
+
+    int width = 10;
+    int height = 5;
+    int area = width * height;
+    int perimeter = 2 * width + 2 * height;
+    std::cout << "area: " << area << '\n';
+    std::cout << "perimeter: " << perimeter << '\n';
 
     return 0;
 }
