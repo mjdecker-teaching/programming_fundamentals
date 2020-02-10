9336f72f7c3c3aa04adb908630216072a2265543
Michael J. Decker, Ph.D <mdecke@bgsu.edu>
2020-02-10 13:22:07 -0800

    Finish coffee order

--- coffee-order/coffee_order.cpp
+++ coffee-order/coffee_order.cpp
@@ -8,6 +8,10 @@
  */
 #include <iostream>
 
+const double COFFEE_PRICE = 2.15;
+const double HOT_CHOCOLATE_PRICE = 2.45;
+const double SCONE_PRICE = 5.59;
+const double BG_TAX_RATE = .08;
 int main() {
 
     std::cout << "How many coffee(s)? ";
@@ -22,5 +26,15 @@ int main() {
     int num_scones;
     std::cin >> num_scones;
 
+    double subtotal = COFFEE_PRICE * num_coffee
+                    + HOT_CHOCOLATE_PRICE * num_hot_chocolate
+                    + SCONE_PRICE * num_scones;
+    double tax = subtotal * BG_TAX_RATE;
+    double total = subtotal * (1 + BG_TAX_RATE);
+
+    std::cout << "subtotal: " << subtotal << '\n';
+    std::cout << "tax: " << tax << '\n';
+    std::cout << "total: " << total << '\n';
+ 
     return 0;
 }
