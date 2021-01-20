4889b18ae31f801810457450dc9eea7af81d38ab
Michael J. Decker, Ph.D <mdecke@bgsu.edu>
2021-01-20 16:41:50 -0500

    Add lecture notes instead of what was in there

--- scope/scope.cpp
+++ scope/scope.cpp
@@ -1,59 +1,83 @@
 /**
- * @file scope.cpp
+ * @file main.cpp
  *
- * Illustrates scoping
+ * Expand about scoping
  *
- * @author Michael John Decker, Ph.D.
- * @date 2/28/2020
+ * @author Michael John Decker, Ph.D. <mdecke@bgsu.edu>
  */
 
 #include <iostream>
+#include <cmath>
 
-// global variables accessible anywhere
-// in file after declared
+// review declaration syntax part in []
+// is optional
+//type variable_name [= intial_value];
+// or
+//type variable_name[(intial_value)];
+
+// If not in any block {} global scope
+// Globals are accessible anywhere in file after declared
+// Usually, constant (read-only)
+// Writing to constants can make
+// Hard to debug programs
 const double SPEED_OF_LIGHT = 3e8;
+//const double SPEED_OF_LIGHT = 3 * pow(10, 8);
 const double SPEED_OF_LIGHT_SQUARED = SPEED_OF_LIGHT * SPEED_OF_LIGHT;
 
-// global mass
-double mass = 9001;
-
+// not a good place for this
+// but helps with understanding scope
+double mass = 1000;
 int main() {
 
-    std::cout << "speed of light: " << SPEED_OF_LIGHT << '\n';
-    std::cout << "speed of light squared: " << SPEED_OF_LIGHT_SQUARED << '\n';
+	std::cout << "speed of light: " << SPEED_OF_LIGHT << '\n';
+	std::cout << "speed of light squared: " << SPEED_OF_LIGHT_SQUARED << '\n';
 
-    // local variable "function mass", hides global
-    double mass = 120;
-    if (mass > 0) {
-        double energy = mass * SPEED_OF_LIGHT_SQUARED;
-        std::cout << "E = " << energy << '\n';
+	// inside of main or another function 
+	// (functions are topic latter in semester)
+	// is local scope.  They exist from where declared
+	// to the ending } in which they were defined
 
-        // can't redefine, but can assign
-        //double energy;
-        energy = 2;
+	// This variable exists until end of } ending main
+	double mass = 120;
 
-        // expression block
-        {
-            // this mass is hiding the one outside
-            double mass = 0;
-            energy = mass * SPEED_OF_LIGHT;
-            std::cout << "E = " << energy << '\n';
+	if(mass > 0) {
+		// if-statements and other constructors 
+		// that can have a {} introduce a new scope
+		// Variables in parent scopes (scopes surrouding it)
+		// can be accessed
+		// variables declared in this scope exist until end of scope
+		// defined (e.g., engery ends at } ending if-statement)
+		double energy = mass * SPEED_OF_LIGHT_SQUARED;
+		std::cout << "E = " << energy << '\n';
 
-            // :: is scope resolution operator
-            energy = ::mass * SPEED_OF_LIGHT;
-            std::cout << "E = " << energy << '\n';
 
-        } // expression block mass ends here
+		// declaring a variable with same name in same scope causes an
+		// compile error
+		//double energy = 0;
 
-        // what is currently in scope
-        // function mass is in scope
-        energy = mass * SPEED_OF_LIGHT;
-        std::cout << "E = " << energy << '\n';
+		// declaring a variable with same name as one from surrounding scope
+		// hides the outter variable
+		double mass = 0;
+		energy = mass * SPEED_OF_LIGHT_SQUARED;
+		std::cout << "E = " << energy << '\n';
 
-    } // energy goes out of scope here
+		// just using {} introduces a new scope
+		// called expression block
+		{
+			// accessing variable from if-statement scope
+			mass = 12;
+			double energy = mass * SPEED_OF_LIGHT_SQUARED;
+			std::cout << "E = " << energy << '\n';
 
-    // can't access as out of scope
-    //std::cout << energy << '\n';
+			// :: means access global scope
+			energy = ::mass * SPEED_OF_LIGHT_SQUARED;
+			std::cout << "E = " << energy << '\n';
+		} // end of scope for expression block energy
+
+	} // end of scope for if-statement energy and mass
 
     return 0;
-} // function mass no longer in scope
+} // end of scope for main mass
+
+// if defined here, can use in main()
+// const double SPEED_OF_LIGHT = 3e8;
